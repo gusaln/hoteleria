f23f3588f6b276681a78384a5c8736fbef0af67a
Gustavo López <git.gustavolopez.xyz@gmail.com>
2023-09-26 14:54:21 -0400

    Modifica estructura de configuraciones

--- config.py
+++ config.py
@@ -5,6 +5,14 @@ import sys
 CURRENT_DIR = os.path.dirname(__file__)
 CONFIG_FILE = os.path.join(CURRENT_DIR, "data", "config.json")
 
+class Config():
+    """Representa la configuración del sistema"""
+
+    def __init__(self, cadena: str, archivo_hoteles: str, archivo_reservaciones: str, archivo_clientes: str):
+        self.cadena = cadena
+        self.archivo_hoteles = archivo_hoteles
+        self.archivo_reservaciones = archivo_reservaciones
+        self.archivo_clientes = archivo_clientes
 
 def leer_config():
     """Lee el archivo de configuración"""
@@ -13,4 +21,5 @@ def leer_config():
     if not os.path.exists(path):
         path = os.path.join(CURRENT_DIR, "seed", "config.json")
     with open(CONFIG_FILE) as fp:
-        return json.load(fp)
+        raw = json.load(fp)
+        return Config(raw["cadena"], raw["archivo_hoteles"], raw["archivo_reservaciones"], raw["archivo_clientes"])
